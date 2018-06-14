f16364a3c036c9d6a6367c12a987f6640ed070e4
Simon Perkins <simon.perkins@gmail.com>
2018-06-14 11:42:26 +0200

    Baseline ordering check dask wrapper

--- tricolour/dask.py
+++ tricolour/dask.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -5,15 +7,51 @@ from __future__ import print_function
 from functools import wraps
 
 import dask.array as da
+import numpy as np
 
 from .flagging import sum_threshold_flagger as np_sum_threshold_flagger
 from .stokes import unpolarised_intensity as np_unpolarised_intensity
+from .util import check_baseline_ordering as np_check_baseline_ordering
+
+
+def check_baseline_ordering(ant1, ant2, chunks, g):
+    """
+    Dask wrapper for :func:`~tricolour.util.check_baseline_ordering`
+    """
+
+    # We use top rather than atop because, while
+    # ant1, ant2 and chunks will have the same number of chunks,
+    # the size of each chunk is different
+    token = da.core.tokenize(ant1, ant2, chunks, g)
+    name = '-'.join(("check-baseline-ordering", token))
+    dims = ("row",)
+
+    dsk = da.core.top(np_check_baseline_ordering, name, dims,
+                        ant1.name, dims,
+                        ant2.name, dims,
+                        chunks.name, dims,
+                        numblocks={
+                            ant1.name: ant1.numblocks,
+                            ant2.name: ant2.numblocks,
+                            chunks.name: chunks.numblocks,
+                        },
+                        g=g)
+
+    dsk.update(ant1.__dask_graph__())
+    dsk.update(ant2.__dask_graph__())
+    dsk.update(chunks.__dask_graph__())
+
+    return da.Array(dsk, name, chunks.chunks, dtype=np.bool)
 
 
 def sum_threshold_flagger(vis, flag, chunks, **kwargs):
     """
     Dask wrapper for :func:`~tricolour.flagging.sum_threshold_flagger`
     """
+
+    # We use top rather than atop because, while
+    # ant1, ant2 and chunks will have the same number of chunks,
+    # the size of each chunk is different
     token = da.core.tokenize(vis, flag, chunks, kwargs)
     name = '-'.join(('flagger', token))
     dims = ("row", "chan", "corr")
