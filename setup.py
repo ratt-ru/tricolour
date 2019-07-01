e8ecef86550a52fd35bb4902c1bfc25a394ec710
Simon Perkins <simon.perkins@gmail.com>
2019-07-01 17:07:54 +0200

    Python 3 upgrade (#28)

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@
 """The setup script."""
 
 from os.path import join as pjoin
+import sys
 
 from setuptools import setup, find_packages
+
+if sys.version_info.major < 3:
+    raise ValueError("python 2 is not supported")
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -23,9 +27,7 @@ requirements = [
     'zarr >= 2.3.1'
 ]
 
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest']
+extras_require = {'testing': ['pytest']}
 
 setup(
     author="Simon Perkins",
@@ -34,8 +36,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -44,15 +44,13 @@ setup(
     entry_points={
         'console_scripts': ['tricolour=tricolour.apps.tricolour.app:main'],
     },
+    extras_require=extras_require,
     install_requires=requirements,
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='tricolour',
     name='tricolour',
     packages=find_packages(),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/ska-sa/tricolour',
     version='0.1.0',
     zip_safe=False,
