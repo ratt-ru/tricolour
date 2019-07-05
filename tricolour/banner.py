4b71687a8a7aa643c493e228dc61147b06abe5d2
Simon Perkins <simon.perkins@gmail.com>
2019-07-05 16:13:09 +0200

    A boring banner

--- tricolour/banner.py
+++ tricolour/banner.py
@@ -5,46 +5,44 @@ from __future__ import division
 from __future__ import print_function
 
 
-_BANNER = """
-***********************************************************************************************************************************************
-{0:s} ▄▄▄▄▄▄▄▄▄▄▄ {1:s} ▄▄▄▄▄▄▄▄▄▄▄{2:s}  ▄▄▄▄▄▄▄▄▄▄▄{3:s}  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄
-{0:s}▐░░░░░░░░░░░▌{1:s}▐░░░░░░░░░░░▌{2:s}▐░░░░░░░░░░░▌{3:s}▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
-{0:s} ▀▀▀▀█░█▀▀▀▀{1:s} ▐░█▀▀▀▀▀▀▀█░▌{2:s} ▀▀▀▀█░█▀▀▀▀{3:s} ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌
-{0:s}     ▐░▌    {1:s} ▐░▌       ▐░▌{2:s}     ▐░▌    {3:s} ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌
-{0:s}     ▐░▌    {1:s} ▐░█▄▄▄▄▄▄▄█░▌{2:s}     ▐░▌    {3:s} ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌
-{0:s}     ▐░▌    {1:s} ▐░░░░░░░░░░░▌{2:s}     ▐░▌    {3:s} ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
-{0:s}     ▐░▌    {1:s} ▐░█▀▀▀▀█░█▀▀ {2:s}     ▐░▌    {3:s} ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀
-{0:s}     ▐░▌    {1:s} ▐░▌     ▐░▌  {2:s}     ▐░▌    {3:s} ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▌
-{0:s}     ▐░▌    {1:s} ▐░▌      ▐░▌ {2:s} ▄▄▄▄█░█▄▄▄▄{3:s} ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌
-{0:s}     ▐░▌    {1:s} ▐░▌       ▐░▌{2:s}▐░░░░░░░░░░░▌{3:s}▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
-{0:s}      ▀     {1:s}  ▀         ▀ {2:s} ▀▀▀▀▀▀▀▀▀▀▀ {3:s} ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀
+_BANNER = r"""
+*******************************************************************************
+                  _______   _           _
+                 |__   __| (_)         | |
+                    | |_ __ _  ___ ___ | | ___  _   _ _ __
+                    | | '__| |/ __/ _ \| |/ _ \| | | | '__|
+                    | | |  | | (_| (_) | | (_) | |_| | |
+                    |_|_|  |_|\___\___/|_|\___/ \__,_|_|
 
 Viva la révolution!
 
 SARAO END USER LICENSE AGREEMENT
 
-A DASK distributed RFI flagger by Science Data Processing and Radio Astronomy Research Group
-Copyright 2019 South African Radio Astronomy Observatory (SARAO, SKA-SA). All rights reserved
+A DASK distributed RFI flagger by
+Science Data Processing and the
+Radio Astronomy Research Group
+Copyright 2019 South African Radio Astronomy Observatory (SARAO, SKA-SA).
+All rights reserved
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED GRATIS, "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
-THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. NO PORTION OF THIS SOFTWARE MAY BE REVERSE ENGINEERED OR INCLUDED IN OTHER 
-PRODUCTS WITHOUT PRIOR WRITTEN PERMISSION FROM SARAO. THIS SOFTWARE, EITHER IN PORTION OR IN ITS ENTIRETY, MAY NOT BE SOLD OR OTHERWISE 
-COMMERCIALLY EXPLOITED BY ANY THIRD PARTY
+THE SOFTWARE IS PROVIDED GRATIS, "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+NO PORTION OF THIS SOFTWARE MAY BE REVERSE ENGINEERED OR INCLUDED
+IN OTHER PRODUCTS WITHOUT PRIOR WRITTEN PERMISSION FROM SARAO.
+THIS SOFTWARE, EITHER IN PORTION OR IN ITS ENTIRETY,
+MAY NOT BE SOLD OR OTHERWISE COMMERCIALLY EXPLOITED BY ANY THIRD PARTY
 
 By using this software you, the end user, agree to these terms and conditions
-***********************************************************************************************************************************************
-"""  # noqa
+*******************************************************************************
+"""
 
 
 def banner():
-    RED = '\033[0;31m'
-    WHITE = '\033[0;37m'
-    BLUE = '\033[0;34m'
-    RESET = '\033[0m'
-
-    # make it Frenchy
-    return _BANNER.format(BLUE, WHITE, RED, RESET)
+    return _BANNER
