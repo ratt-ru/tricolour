6daadebf9911bb14958f2de56b59e155f8b3355e
Benna Hugo <bennahugo@aol.com>
2019-07-30 10:01:50 +0200

    fix PR cr

--- docker/release_context/tag_release.sh
+++ docker/release_context/tag_release.sh
@@ -6,13 +6,28 @@ then
 fi
 
 echo "TAGGING NEW RELEASE AS ${1}"
-rm -rf ./rel && rm -rf ./build && rm -rf ./dist && rm -rf ./tmpvenv && rm -f *.spec && rm -f *.log && \
+rm -rf ./rel && \
+rm -rf ./build && \
+rm -rf ./dist && \
+rm -rf ./tmpvenv && \
+rm -f *.spec && \
+rm -f *.log && \
 rsync -av --progress ../../ ./rel --exclude docker --exclude .git --exclude '__pycache__' --exclude '*.pyc' --exclude '*.pyo' && \
-virtualenv -p python3 tmpvenv && . tmpvenv/bin/activate && pip install ./rel[obfuscation] && pip uninstall -y tricolour && \
+virtualenv -p python3 tmpvenv && \
+. tmpvenv/bin/activate && \
+pip install ./rel[obfuscation] && \
+pip uninstall -y tricolour && \
 pyinstaller rel/tricolour/apps/tricolour/tricolourexe.py && \
-mkdir dist/tricolourexe/tricolour && cp -r rel/tricolour/conf dist/tricolourexe/tricolour/conf && cp -r rel/tricolour/data dist/tricolourexe/tricolour/data && \
+mkdir dist/tricolourexe/tricolour && \
+cp -r rel/tricolour/conf dist/tricolourexe/tricolour/conf && \
+cp -r rel/tricolour/data dist/tricolourexe/tricolour/data && \
 rm -rf rel && docker build -f release.docker -t ${1} . && \
-rm -rf ./rel && rm -rf ./build && rm -rf ./dist && rm -rf ./tmpvenv && rm -f *.spec && rm -f *.log && \
+rm -rf ./rel && \
+rm -rf ./build && \
+rm -rf ./dist && \
+rm -rf ./tmpvenv && \
+rm -f *.spec && \
+rm -f *.log && \
 echo "Successfully build tag ${1}" && exit 0
 
 rm -rf ./rel && rm -rf ./build && rm -rf ./dist && rm -rf ./tmpvenv && rm -f *.spec && rm -f *.log && \
