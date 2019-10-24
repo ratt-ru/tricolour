c37ca88c77c343a5d8eb6682ab8172d623d2510a
Benjamin Hugo <bennahugo@gmail.com>
2019-10-24 11:17:43 +0200

    Bump version to 0.1.4 (#62)

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     packages=find_packages(),
     python_requires=">=3.5",
     url='https://github.com/ska-sa/tricolour',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
