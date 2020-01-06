059cc332ab658ab7852f2d34abb682e3cac21ef3
Simon Perkins <simon.perkins@gmail.com>
2020-01-06 14:38:56 +0200

    Set long_description_content_type

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
     'dask[array] >= 2.2.0',
     'donfig >= 0.4.0',
@@ -41,7 +38,8 @@ setup(
     },
     extras_require=extras_require,
     install_requires=requirements,
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='tricolour',
     name='tricolour',
