9b991a1f2861730ce1d1e59f037720d7e5e2e992
Simon Perkins <simon.perkins@gmail.com>
2020-01-06 13:18:07 +0200

    Fix mix of unicode and raw text in banner

--- tricolour/banner.py
+++ tricolour/banner.py
@@ -13,34 +13,8 @@ _BANNER = r"""
                     | | '__| |/ __/ _ \| |/ _ \| | | | '__|
                     | | |  | | (_| (_) | | (_) | |_| | |
                     |_|_|  |_|\___\___/|_|\___/ \__,_|_|
-
+""" + u"""
 Viva la révolution!
-
-SARAO END USER LICENSE AGREEMENT
-
-A DASK distributed RFI flagger by
-Science Data Processing and the
-Radio Astronomy Research Group
-Copyright 2019 South African Radio Astronomy Observatory (SARAO, SKA-SA).
-All rights reserved
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED GRATIS, "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
-THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-NO PORTION OF THIS SOFTWARE MAY BE REVERSE ENGINEERED OR INCLUDED
-IN OTHER PRODUCTS WITHOUT PRIOR WRITTEN PERMISSION FROM SARAO.
-THIS SOFTWARE, EITHER IN PORTION OR IN ITS ENTIRETY,
-MAY NOT BE SOLD OR OTHERWISE COMMERCIALLY EXPLOITED BY ANY THIRD PARTY
-
-By using this software you, the end user, agree to these terms and conditions
-*******************************************************************************
 """
 
 
