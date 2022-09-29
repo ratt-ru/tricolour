423b53e9ca860db993ce469fa6f170551ae27f4c
bennahugo <bennahugo@gmail.com>
2022-09-29 12:35:20 +0200

    Update jenkinsfile

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -18,7 +18,7 @@ TEST_MS_REL=1519747221.subset.ms
 
 docker run \
     --rm \
-    -v $TEST_OUTPUT_DIR:/testdata
+    -v $TEST_OUTPUT_DIR:/testdata \
     --env TRICOLOUR_TEST_MS=/testdata/$TEST_MS_REL \
     --workdir /code \
     --entrypoint /bin/sh \
