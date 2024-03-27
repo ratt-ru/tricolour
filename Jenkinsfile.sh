7ede714854b4f35bb68acb822a48fb0f0d70ff99
Benjamin Hugo <bennahugo@gmail.com>
2024-03-27 15:55:29 +0200

    Drop failing Python3.6 support

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -18,19 +18,6 @@ docker build -f ${WORKSPACE_ROOT}/projects/tricolour/docker/python310.docker -t 
 tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
 TEST_MS_REL=1519747221.subset.ms
 
-# test 3.6
-docker run \
-    --rm \
-    -v $TEST_OUTPUT_DIR:/testdata \
-    --env TRICOLOUR_TEST_MS=/testdata/$TEST_MS_REL \
-    --workdir /code \
-    --entrypoint /bin/sh \
-    tricolour.1804.py36:${BUILD_NUMBER} \
-    -c "python3 -m pytest --flake8 -s -vvv ."
-
-rm -rf $TEST_OUTPUT_DIR/TEST_MS_REL
-tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
-
 # test 3.8
 docker run \
     --rm \
@@ -40,6 +27,9 @@ docker run \
     --entrypoint /bin/sh \
     tricolour.2004.py38:${BUILD_NUMBER} \
     -c "python3 -m pytest --flake8 -s -vvv ."
+
+rm -rf $TEST_OUTPUT_DIR/TEST_MS_REL
+tar xvf $TEST_DATA_DIR/acceptance_test_data.tar.gz -C $TEST_OUTPUT_DIR
 
 # test 3.10
 docker run \
